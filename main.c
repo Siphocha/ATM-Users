26566f5ba80ab0abedffd7dcb1864a995bc27952
Siphocha <siphochakhala@gmail.com>
2025-06-03 19:52:24 +0200

    added data struct for users

--- main.c
+++ main.c
@@ -4,7 +4,14 @@ void checking_balance(float balance);
 //* only for when the function needs to edit value
 void depositing(float *balance);
 void withdrawing(float *balance);
-void users(),
+
+
+struct Users {
+    //establishing user data structure
+    char name[];
+    int password;
+    struct User *next;
+};
 
 int main() {
     //main elements of program will go here.
