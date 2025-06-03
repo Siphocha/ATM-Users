d51ad49304bf2286d75979f4a011ddea1a3f81c0
Siphocha <siphochakhala@gmail.com>
2025-06-03 19:50:53 +0200

    here we go again

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void checking_balance(float balance);
+//* only for when the function needs to edit value
+void depositing(float *balance);
+void withdrawing(float *balance);
+void users(),
+
+int main() {
+    //main elements of program will go here.
+}
