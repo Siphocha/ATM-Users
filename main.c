3078d0b024d78a699386995b47bce1f96317dab2
Siphocha <siphochakhala@gmail.com>
2025-06-06 10:02:19 +0200

    added pre-defined users in simplistic form

--- main.c
+++ main.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 
+//check the balance
 void checking_balance(float balance);
 //* only for when the function needs to edit value
 void depositing(float *balance);
+//depositing and withdrawing function declarations
 void withdrawing(float *balance);
 
 
-struct Users {
-    //establishing user data structure
+struct Users { //THIS MAY BE UNECESSARY AS WE NEED PREDEFINED USERS TECHNICALLY.
+    //establishing user data structure for use later
     char name[];
     int password;
     struct User *next;
 };
 
 int main() {
-    //main elements of program will go here.
+    //Pre-defined user list
+    char username1[] = "Sipho";
+    int password1[] = 1;
+
+    char username2[] = "Tejiri";
+    int password2[] = 2;
+
+    char username3[] = "Shafii";
+    int passsword3[] = 3;
+
+
+
 }
